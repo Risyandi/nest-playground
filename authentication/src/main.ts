0afd5bc891b1183cc06bc5e97ad951269eb9ee3c
Risyandi <risyandi.94@gmail.com>
2024-11-21 09:25:09 +0700

    fix(main): adding cors and logs

--- authentication/src/main.ts
+++ authentication/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //* allow Cross-origin resource sharing (CORS) is a mechanism that allows resources to be requested from another domain.
+  app.enableCors();
+
+  //* start server
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Server running on port ${process.env.PORT ?? 3000}`);
 }
 bootstrap();
