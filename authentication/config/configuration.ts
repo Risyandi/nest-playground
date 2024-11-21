2d050ad74f6819da730134404d71209f2098edef
Risyandi <risyandi.94@gmail.com>
2024-11-21 09:59:51 +0700

    fix(configuration): remove

--- authentication/config/configuration.ts
+++
@@ -1,7 +0,0 @@
-//* adding here environment variables for the example url of services API
-export default () => ({
-  baserUrl: 'http://localhost',
-  servicesUrl: {
-    servicesName: 'http://localhost/v1/api/services-name',
-  },
-});
