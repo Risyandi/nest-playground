5248ba847ac4fd2bf180eac61bc683ec162977d7
Risyandi <risyandi.94@gmail.com>
2024-11-21 11:22:24 +0700

    fix(version): adding example versioning

--- authentication/src/app.controller.ts
+++ authentication/src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Version } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // get hello version 1
+  @Version('1')
+  @Get('hello')
+  getHelloV1(): string {
+    return this.appService.getHello('#1');
+  }
+
+  // get hello version 2
+  @Version('2')
+  @Get('hello')
+  getHelloV2(): string {
+    return this.appService.getHello('#2');
   }
 }
