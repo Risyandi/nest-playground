3b86e65f1e03670ac9d5166477e5f45ce9db83c1
Risyandi <risyandi.94@gmail.com>
2024-11-21 11:21:11 +0700

    fix(versioning): adding versioning config

--- authentication/src/main.ts
+++ authentication/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //* allow Cross-origin resource sharing (CORS) is a mechanism that allows resources to be requested from another domain.
   app.enableCors();
+
+  //* enable versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
 
   //* start server
   await app.listen(process.env.PORT ?? 3000);
