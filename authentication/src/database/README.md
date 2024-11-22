a5fb08d6be650c4d91c7a95819f2cf885124459e
Risyandi <risyandi.94@gmail.com>
2024-11-22 22:13:07 +0700

    fix(database): adding folder and file

---
+++ authentication/src/database/README.md
@@ -0,0 +1,3 @@
+# Directory `database`
+
+`database` This directory contains all of the database-related files, such as entity definitions, migrations, and seed data.
---
+++ authentication/src/database/database.providers.ts
@@ -0,0 +1,10 @@
+import * as mongoose from 'mongoose';
+import config from '../config/configuration';
+
+export const databaseProviders = [
+  {
+    provide: 'MONGO_DATABASE_CONNECTION',
+    useFactory: async (): Promise<typeof mongoose> =>
+      await mongoose.connect(config().mongodbUrl),
+  },
+];
