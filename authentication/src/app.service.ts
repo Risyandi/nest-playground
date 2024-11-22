af676551a1c3eef092736684b658389471d7dcc6
Risyandi <risyandi.94@gmail.com>
2024-11-22 16:40:56 +0700

    fix(app): remove file and relocation

--- authentication/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(version: string): string {
-    return `Hello World! version ${version}`;
-  }
-}
