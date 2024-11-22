4ba926db643ef32757d7d9f240743f4d04abe7ff
Risyandi <risyandi.94@gmail.com>
2024-11-22 22:12:43 +0700

    fix(core): adding folder and file

---
+++ authentication/src/core/README.md
@@ -0,0 +1,3 @@
+# Directory `core`
+
+`core` This directory contains all of the core code that can be used by multiple modules in your application. This includes things like custom exceptions, filters, guards, interfaces, middlewares, pipes, providers, and utilities.
---
+++ authentication/src/core/core.module.ts
@@ -0,0 +1,41 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { WelcomeModule } from '../app/modules/welcome/welcome.module';
+import { AuthModule } from '../app/modules/auth/auth.module';
+import Joi from 'joi';
+
+//* uncomment if you want to load configuration from file [configuration.ts]
+import configuration from '../config/configuration';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      //* set validation options
+      validationSchema: Joi.object({
+        PORT: Joi.number().port().default(3000),
+      }),
+
+      //* unknown environment variables will throw an error
+      // validationOptions: {
+      //   allowUnknown: false,
+      //   abortEarly: true,
+      // },
+
+      //* set configuration use in other modules
+      isGlobal: true,
+
+      //* uncomment if you want to ignore use env file
+      // ignoreEnvFile: true,
+
+      //* uncomment if you want to custom set file path based on environment
+      // envFilePath: ['.env.development.local', '.env.development'],
+
+      //* uncomment if you want to load configuration from file [configuration.ts]
+      //  allowing you to load multiple configuration file ex: [databaseConfig, authConfig]
+      load: [configuration],
+    }),
+    AuthModule,
+    WelcomeModule,
+  ],
+})
+export class CoreModule {}
